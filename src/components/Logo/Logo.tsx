d8b66aecf963079e18df3c32527b022b14bc520e
tiagofbsantos <santosfbtiago@gmail.com>
2023-07-09 15:42:17 +0200

    small refactor of Logo component.

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -2,20 +2,18 @@ import { Tilt } from "react-tilt";
 import vision from "../../images/vision.png";
 import "./Logo.css";
 
-const Logo = () => {
-  return (
-    <div className="ma4 mt0">
-      <Tilt
-        className="Tilt br2 shadow-2"
-        options={{ max: 55 }}
-        style={{ height: 150, width: 150 }}
-      >
-        <div className="Tilt-inner">
-          <img style={{ paddingTop: "12px" }} alt="logo" src={vision} />
-        </div>
-      </Tilt>
-    </div>
-  );
-};
+const Logo = () => (
+  <div className="ma4 mt0">
+    <Tilt
+      className="Tilt br2 shadow-2"
+      options={{ max: 55 }}
+      style={{ height: 150, width: 150 }}
+    >
+      <div className="Tilt-inner">
+        <img style={{ paddingTop: "12px" }} alt="logo" src={vision} />
+      </div>
+    </Tilt>
+  </div>
+);
 
 export default Logo;
