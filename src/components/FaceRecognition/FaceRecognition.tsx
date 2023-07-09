efc57773a5341574d53bc842b1d678823f8ad3fa
tiagofbsantos <santosfbtiago@gmail.com>
2023-07-09 15:36:42 +0200

    small refactor of FaceRecognition.

--- src/components/FaceRecognition/FaceRecognition.tsx
+++ src/components/FaceRecognition/FaceRecognition.tsx
@@ -14,40 +14,38 @@ interface Props {
   boxes: Box[];
 }
 
-const FaceRecognition = ({ imageUrl, boxes }: Props) => {
-  return (
-    <div className="center">
-      <div className="absolute mv8">
-        <img
-          id="inputimage"
-          alt=""
-          src={imageUrl}
-          width="500px"
-          height="auto"
-        />
-        {boxes.map((box, i) => {
-          return (
-            <div
-              key={i}
-              className="bounding-box"
-              style={{
-                top: box.topRow,
-                right: box.rightCol,
-                bottom: box.bottomRow,
-                left: box.leftCol,
-              }}
-            >
-              <div key={`d${i}`} className="celebDescription">
-                <span key={`s${i}`}>
-                  {box.name}, {Math.trunc(box.certainty * 100)}% sure
-                </span>
-              </div>
+const FaceRecognition = ({ imageUrl, boxes }: Props) => (
+  <div className="center">
+    <div className="absolute mv8">
+      <img
+        id="inputimage"
+        alt=""
+        src={imageUrl}
+        width="500px"
+        height="auto"
+      />
+      {boxes.map((box, i) => {
+        return (
+          <div
+            key={i}
+            className="bounding-box"
+            style={{
+              top: box.topRow,
+              right: box.rightCol,
+              bottom: box.bottomRow,
+              left: box.leftCol,
+            }}
+          >
+            <div key={`d${i}`} className="celebDescription">
+              <span key={`s${i}`}>
+                {box.name}, {Math.trunc(box.certainty * 100)}% sure
+              </span>
             </div>
-          );
-        })}
-      </div>
+          </div>
+        );
+      })}
     </div>
-  );
-};
+  </div>
+);
 
 export default FaceRecognition;
